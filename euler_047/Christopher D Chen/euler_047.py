926134e60f80707e058953e4aef7a0ae95a6a28d
thecdc <christoipher.chen@gmail.com>
2017-02-22 18:10:37 -0800

    formatting

--- euler_047/Christopher D Chen/euler_047.py
+++ euler_047/Christopher D Chen/euler_047.py
@@ -1,13 +1,31 @@
+#!/usr/bin/env python3
+"""
+Distinct primes factors
+Problem 47
+
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
+"""
 from utils import primeFactors
 
 found = []
 
 i = 1
 while len(found) < 4:
-	i+= 1
-	if len(set(primeFactors(i))) == 4:
-		found.append(i)
-	else:
-		found = []
+    i+= 1
+    if len(set(primeFactors(i))) == 4:
+        found.append(i)
+    else:
+        found = []
 
 print(found[0])
