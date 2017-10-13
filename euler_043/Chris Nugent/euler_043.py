b0b22e91b446852a2f61d1f83ee187f1cdd8f736
Christopher Nugent <awedeven+github@gmail.com>
2017-10-13 00:13:26 -0700

    Now uses a generator

--- euler_043/Chris Nugent/euler_043.py
+++ euler_043/Chris Nugent/euler_043.py
@@ -1,19 +1,14 @@
 from collections import deque
 
-sum = 0
-
-def iterate_nums(current, remaining):
-	if current > 10 ** 9:
-		if check_num(current, 1, 3, deque([2, 3, 5, 7, 11, 13, 17])):
-			global sum
-			sum += current
-
-	for number in remaining:
+def get_pandigitals(current = 0, remaining = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}):
+	if not remaining:
+		yield current
+	for number inremaining:
 		if current == 0 and number == 0:
 			continue
 		current *= 10
 		current += number
-		iterate_nums(current, remaining - {number})
+		yield from get_pandigitals(current, remaining - {number})
 		current //= 10
 
 def check_num(num, start, length, div):
@@ -26,8 +21,10 @@ def check_num(num, start, length, div):
 		return False
 
 def main():
-	numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	iterate_nums(0, numbers)
+	sum = 0
+	for pandigital in get_pandigitals():
+		if check_num(pandigital, 1, 3, [2, 3, 5, 7, 11, 13, 17]):
+			sum += pandigital
 	print(sum)
 
 if __name__ == "__main__":
