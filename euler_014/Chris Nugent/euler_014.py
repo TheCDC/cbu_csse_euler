c185ba245b11fab4f84ebfe235e2650b933bf0d6
Christopher Nugent <awedeven+github@gmail.com>
2017-10-24 20:52:46 -0700

    Fixed output

--- euler_014/Chris Nugent/euler_014.py
+++ euler_014/Chris Nugent/euler_014.py
@@ -9,13 +9,16 @@ def chain_length(value: int) -> int:
     elif value % 2 == 0:
         return 1 + chain_length(value // 2)
     else:
-        return 1 + chain_length(3 * value + 1)
+        return 2 + chain_length((3 * value + 1) // 2)
 
+def process_num(value: int) -> (int, int):
+	#print(chain_length.cache_info())
+	return (value, chain_length(value))
 
 def main():
     pool = multiprocessing.Pool()
-    result = pool.map(chain_length, range(1, 10**6))
-    print(max(result))
+    result = pool.map(process_num, range(1, 10**6))
+    print(max(result, key=lambda t:t[1]))
 
 
 if __name__ == '__main__':
