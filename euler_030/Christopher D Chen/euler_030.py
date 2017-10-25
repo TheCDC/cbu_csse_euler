0ded572f0f3e4808b22d96a6445f9b0fe6554b5e
thecdc <christoipher.chen@gmail.com>
2017-10-24 17:46:48 -0700

    Comments

--- euler_030/Christopher D Chen/euler_030.py
+++ euler_030/Christopher D Chen/euler_030.py
@@ -14,15 +14,19 @@ def main():
     # find an upper bound of number of digits
     nd = 1
     n = 2
-    # find *a* ceiling for numbers that fit the criteria
+
+    """find *a* ceiling for numbers that fit the criteria
+    AKA make 9, 99, 999 until it's bigger than the
+    function of its digits"""
+
     while n < ds(n):
 
         nd += 1
-        n = n * 10 + 9
+        n = n * 10 + 9  # add nine to produce 9, 99, 999, etc.
     # lower that ceiling until the real one is found
     while n != ds(n):
         n -= 1
-    # print("Upper (inclusive):",n)
+    print("Largest number equal to the function of its digits:", n)
     # don't include 1
     print(sum([i for i in range(2, n + 1) if i == ds(i)]))
 
