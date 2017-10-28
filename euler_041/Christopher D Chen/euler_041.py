dcb8c4e3a062db7429f786afa2958d6e1ca8c32c
thecdc <christoipher.chen@gmail.com>
2017-10-28 00:05:52 -0700

    Solve 43

---
+++ euler_041/Christopher D Chen/euler_041.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from itertools import permutations
+import multiprocessing as mp
+
+
+def generate_pandigitals(start=1, stop=None):
+    if stop is None:
+        stop = 10
+    for i in range(start, stop):
+        yield from permutations(set(range(i)))
+
+
+def list_to_num(l, b=10):
+    n = 0
+    for i in l:
+        n = n * b + i
+    return n
+
+
+def isPrime(n):
+    """Return whether a number is prime."""
+    # handle base cases
+    if n <= 0:
+        return False
+    elif n == 2:
+        return True
+    elif n == 1:
+        return False
+    for i in range(2, int(n**(1 / 2)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def process_num(digits):
+    n = list_to_num(digits)
+    if isPrime(n):
+        return n
+    else:
+        return 0
+
+
+def main():
+    pool = mp.Pool()
+    results = pool.map(process_num, generate_pandigitals())
+    print(max(results))
+    # print(max(map(process_num, generate_pandigitals())))
+
+if __name__ == '__main__':
+    main()
